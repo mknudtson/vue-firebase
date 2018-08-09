28e4cde161e20dc27c4ac8f83f8392a657ea81f4
Melissa Knudtson <melissa.erin.knudtson@gmail.com>
2018-08-09 09:46:12 -0400

    Lectures 27-29: What is the Vue Router?, Setting up Routes and Router Links

---
+++ routing/src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="about">
+        <h1>About</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data (){
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
+
+
+
+
---
+++ routing/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="main-nav">
+        <ul>
+            <li><router-link to='/'>Home</router-link></li>
+            <li><router-link :to="{ name: 'About'}">About</router-link></li>
+            <!-- to change where this link leads, just go to index.js and update the path  -->
+            <!-- this is useful if you have the same link in a lot of places - just update it from one central location to update it everywhere -->
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+ul{
+    list-style-type: none;
+    padding: 0;
+}
+a { /* because router links become anchor tags */
+    color: #42b983;
+}
+</style>
+

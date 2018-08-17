91a378e6ca355d7c42e4294f8fc94a890f90ac8e
Melissa Knudtson <melissa.erin.knudtson@gmail.com>
2018-08-16 22:02:03 -0400

    Lecture 31: Watching the  Object

--- routing/src/components/ViewProfile.vue
+++ routing/src/components/ViewProfile.vue
@@ -10,17 +10,19 @@ export default {
     data() {
         return {
             userId: this.$route.params.user_id // see index.js file - route features :userId where ":" indicates parameter - this binds the url for the user's profile page to the user's username 
-            // params is built in
-            // $route = current route (e.g. .../profile/123)
-            // user_id = name given to route parameter inside router>index.js 
-            // user-id is "declared" in router>index (":user_id" as a parameter)
-            // for now, we can type anything in after /profile/ in the url to be the user ID
-            // i am sure that when we have a database of real users it will not be this easy 
-            // note if you jump directly from one profile to another the app does not update
-            // why? b/c vue knows that we want the same component
-            // so it recycles it
-            // but it's using the same data - not being destroyed / not being recreated
+            // to get around plm of not being able to jump directly from profile to profile...
+            // ... ask vue to watch $route object (below)
+            // when it changes, update the id and therefore the template
+            // this is simply a workaround for the way that vue recycles components rather than recreate or destroy them
         }
+    },
+    methods: {
+        updateId(){ // this function updates userId ... and it rapidly updates! (seems like a lot of work for something that i would think should be automatic)
+            this.userId = this.$route.params.user_id
+        }
+    },
+    watch: {
+        $route: 'updateId' // when $route changes, fire this function
     }
 }
 </script>

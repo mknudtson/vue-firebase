eb97feeebeb67af9502a4997c00e0fa861699bff
Melissa Knudtson <melissa.erin.knudtson@gmail.com>
2018-08-16 21:39:16 -0400

    Lecture 30: Route Parameters (w/extra notes)

--- routing/src/components/ViewProfile.vue
+++ routing/src/components/ViewProfile.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="view-profile">
-        <h2>Profile for {{ userId }}</h2>
+        <h2>Profile for {{ userId }}</h2> <!-- from data property below (NOT parameter directly - they're a little different) -->
     </div>
 </template>
 
@@ -9,7 +9,17 @@ export default {
     name: 'ViewProfile',
     data() {
         return {
-            userId: this.$route.params.user_id
+            userId: this.$route.params.user_id // see index.js file - route features :userId where ":" indicates parameter - this binds the url for the user's profile page to the user's username 
+            // params is built in
+            // $route = current route (e.g. .../profile/123)
+            // user_id = name given to route parameter inside router>index.js 
+            // user-id is "declared" in router>index (":user_id" as a parameter)
+            // for now, we can type anything in after /profile/ in the url to be the user ID
+            // i am sure that when we have a database of real users it will not be this easy 
+            // note if you jump directly from one profile to another the app does not update
+            // why? b/c vue knows that we want the same component
+            // so it recycles it
+            // but it's using the same data - not being destroyed / not being recreated
         }
     }
 }
--- routing/src/router/index.js
+++ routing/src/router/index.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home' // @/ meaning root directory / src fldr
 import About from '@/components/About'
-import ViewProfile from '@/components/ViewProfile'
+import ViewProfile from '@/components/ViewProfile' // import the component
 
-Vue.use(Router)
+Vue.use(Router) // similar to dragula set-up
 
 export default new Router({
   routes: [
@@ -18,8 +18,8 @@ export default new Router({
       name: 'About',
       component: About
     },
-    {
-      path: '/profile/:user_id', // : to indicate parameter
+    { // create the path, name and component to load
+      path: '/profile/:user_id', // ":" indicates parameter - is implicitly a variable
       name: 'ViewProfile',
       component: ViewProfile
     }

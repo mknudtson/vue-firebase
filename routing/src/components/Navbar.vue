3556cc1e9fea68c73f2e2e0fec44dbfc0a09ddc2
Melissa Knudtson <melissa.erin.knudtson@gmail.com>
2018-08-20 10:19:10 -0400

    Lecture 32: More on Router Links

--- routing/src/components/Navbar.vue
+++ routing/src/components/Navbar.vue
@@ -6,6 +6,21 @@
             <!-- to change where this link leads, just go to index.js and update the path  -->
             <!-- this is useful if you have the same link in a lot of places - just update it from one central location to update it everywhere -->
         </ul>
+        <h2>User Profiles</h2>
+        <ul>
+            <li v-for="(id, index) in userIds" :key="index">
+            <!-- don't forget, to assign index value to key binding, pass in the index -->
+                <router-link :to="{ name: 'ViewProfile', params: { user_id: id } }">
+                <!-- see router > index.js -->
+                <!-- name refers to component -->
+                <!-- params refers to params (built in) - user_id binding is created in 'path' property for ViewProfile router link in index.js file -->
+                    <span>Profile {{ id }} </span>
+                    <!-- for some looped output - all the profiles -->
+                    <!-- this has nothing to do with how user profiles are actually loaded so just keep that in mind -->
+                </router-link>
+            </li>
+
+        </ul>
     </nav>
 </template>
 
@@ -14,7 +29,7 @@ export default {
     name: 'Navbar',
     data () {
         return {
-
+            userIds: ['1', '2', '3', '4']
         }
     }
 }
